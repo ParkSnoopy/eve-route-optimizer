728551c9ebd9b6e7b59a70d275610bacfcce24e1
ParkSnoopy <sunwoo2539@gmail.com>
2024-10-31 11:33:15 +0000

    ADD: build script

---
+++ build.rs
@@ -0,0 +1,16 @@
+use anyhow::Result;
+
+
+fn main() -> Result<()> {
+    #[cfg(windows)] // Build host machine have to be WindowsOS in order to compile with manifest/icon
+    {
+        use winres;
+
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("assets/icon.ico");
+
+        res.compile()?;
+    }
+
+    Ok(())
+}
