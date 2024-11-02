555145d6f74ba330feba1f0c612d84f8d7b0b045
ParkSnoopy <117149837+ParkSnoopy@users.noreply.github.com>
2024-11-02 16:05:13 +0900

    add `Heatsink` struct
    ADD: `Heatsink` struct to implement request lock after certain amount of request in order to avoid high bandwidth usage and request blocking from server

--- src/state/fetch.rs
+++ src/state/fetch.rs
@@ -1,17 +1,75 @@
 use crate::{
     config,
     utils,
+    trace,
 };
 
 use std::cmp::max;
 use std::iter::repeat;
+use std::thread::sleep;
+use std::time::Duration;
 
 use nu_ansi_term::Color;
 
 
 
+struct Heatsink {
+    pub temp: usize,
+    overheated: bool,
+}
+
+fn cooldown() {
+    sleep(Duration::from_millis(config::COOLDOWN_INTERVAL_MILLS));
+}
+
+impl Heatsink {
+    fn new() -> Heatsink {
+        Heatsink {
+            temp: 0,
+            overheated: false,
+        }
+    }
+
+    fn wait(&mut self) {
+        //trace::info(format!("Wait on heatsink ( heat: {} )", self.temp));
+
+        if !self.overheated && self.temp >= config::REQUEST_OVERHEAT {
+            self.overheated = true;
+            //trace::warn("Heatsink overheated!");
+        }
+
+        if self.overheated {
+            loop {
+                trace::info(format!("Heatsink cooling... ( heat: {} )", self.temp));
+
+                self.temp -= 1;
+                cooldown();
+
+                if self.temp <= 0 {
+                    self.overheated = false;
+                    trace::clear();
+                    break
+                }
+            };
+        }
+
+        //trace::info("Passed heatsink");
+        //cooldown();
+        self.temp += 1;
+        //trace::info(format!("Current temp: {}", self.temp));
+        //cooldown();
+    }
+
+    fn temp_threshold_percentile(&self) -> usize {
+        100 * self.temp / config::REQUEST_OVERHEAT
+    }
+}
+
+
 pub struct FetchState {
     init: bool,
+    heatsink: Heatsink,
+
     total: usize,
     fetching: usize,
     success: usize,
@@ -22,6 +80,8 @@ impl FetchState {
     pub fn new() -> FetchState {
         FetchState {
             init: false,
+            heatsink: Heatsink::new(),
+
             total: 0,
             fetching: 0,
             success: 0,
@@ -71,6 +131,8 @@ impl FetchState {
     }
 
     pub fn add_fetching(&mut self) {
+        self.heatsink.wait();
+
         if self.init {
             self.fetching += 1;
         }
